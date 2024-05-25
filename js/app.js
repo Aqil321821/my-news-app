f2babce8d4d14d652af20b07222e3fb3359bfd66
Aqil321821 <aqilrao321821@gmail.com>
2024-05-25 10:33:15 +0500

    search functionality done
    waiting to test when api gets reset

--- js/app.js
+++ js/app.js
@@ -4,8 +4,13 @@ const global = {
   currentPage: window.location.pathname,
   api: {
     API_URL: 'https://newsapi.org/v2/',
-    API_KEY: '3029b7a3f4014c23a88c9c0dc26efe2f',
+    API_KEY: '7b60e8f541cf46f687cf8bfd61d3e07a',
   },
+  search: {
+    term: '',
+    page: 1,
+    totalResults: 1,
+  }
 };
 
 
@@ -29,7 +34,7 @@ const global = {
 async function getUSNews() {
 
   const { articles } = await fetchAPIData('top-headlines?country=us');
-  const filteredArticles = articles.filter(articles => articles.content !== '[Removed]' || articles.content !==null);
+  const filteredArticles = articles.filter(articles => articles.content !== '[Removed]' && articles.content !== null);
   console.log(filteredArticles);
   filteredArticles.forEach(article => {
     console.log(typeof article.content);
@@ -49,7 +54,7 @@ async function getUSNews() {
       } 
            <div class="card-text d-flex news-options">
              <p class="card-text me-2 pe-2 custom-border"><small class="text-muted">By : ${article.source.name}</small></p>
-             <p class="card-text "><small class="text-muted">${article.publishedAt.slice(0, 9)}</small></p>
+             <p class="card-text "><small class="text-muted">${article.publishedAt.slice(0, 10).split('-').reverse().join('-')}</small></p>
            </div>
          
          </div>
@@ -69,8 +74,8 @@ async function getBBCNews() {
   const { articles } = await fetchAPIData('top-headlines?sources=bbc-news');
   const filteredArticles = articles.filter(articles => articles.content !== '[Removed]');
   console.log(filteredArticles);
-filteredArticles.forEach(article => {
-   console.log(typeof article.content);
+  filteredArticles.forEach(article => {
+    console.log(typeof article.content);
     const div = document.createElement('div');
     div.classList.add('col-3');
     div.innerHTML = `
@@ -80,14 +85,14 @@ filteredArticles.forEach(article => {
          <div class="card-body">
              <a href="${article.url}" target="_blank">
               ${article.urlToImage
-                ?`<img src="${article.urlToImage}" class="card-img-top object-fit-cover">`:`<img src="../img/no-image.png" class="card-img-top object-fit-cover">`
-              }
-           ${typeof article.content === "object"?
-            `<p class="card-text mt-2">Please Click to Read more...</p>`:`<p class="card-text mt-2">${article.content.slice(0,75)} ...Read more</p>`
-           } 
+        ? `<img src="${article.urlToImage}" class="card-img-top object-fit-cover">` : `<img src="../img/no-image.png" class="card-img-top object-fit-cover">`
+      }
+           ${typeof article.content === "object" ?
+        `<p class="card-text mt-2">Please Click to Read more...</p>` : `<p class="card-text mt-2">${article.content.slice(0, 75)} ...Read more</p>`
+      } 
            <div class="card-text d-flex news-options">
              <p class="card-text me-2 pe-2 custom-border"><small class="text-muted">By : ${article.source.name}</small></p>
-             <p class="card-text "><small class="text-muted">${article.publishedAt.slice(0, 9)}</small></p>
+             <p class="card-text "><small class="text-muted">${article.publishedAt.slice(0, 10).split('-').reverse().join('-')}</small></p>
            </div>
          
          </div>
@@ -96,8 +101,8 @@ filteredArticles.forEach(article => {
          
          `;
     document.querySelector('.row').appendChild(div);
-  
-      }); 
+
+  });
 
 }
 
@@ -106,10 +111,10 @@ filteredArticles.forEach(article => {
 async function getBuisnessNews() {
 
   const { articles } = await fetchAPIData('top-headlines?country=us&category=business');
-  const filteredArticles = articles.filter(articles => articles.content !== '[Removed]' || articles.content ===null);
+  const filteredArticles = articles.filter(articles => articles.content !== '[Removed]' || articles.content === null);
   console.log(filteredArticles);
-filteredArticles.forEach(article => {
-   console.log(typeof article.content);
+  filteredArticles.forEach(article => {
+    console.log(typeof article.content);
     const div = document.createElement('div');
     div.classList.add('col-3');
     div.innerHTML = `
@@ -119,14 +124,14 @@ filteredArticles.forEach(article => {
          <div class="card-body">
              <a href="${article.url}" target="_blank">
               ${article.urlToImage
-                ?`<img src="${article.urlToImage}" class="card-img-top object-fit-cover">`:`<img src="../img/no-image.png" class="card-img-top object-fit-cover">`
-              }
-           ${typeof article.content === "object"?
-            `<p class="card-text mt-2">Please Click to Read more...</p>`:`<p class="card-text mt-2">${article.content.slice(0,75)} ...Read more</p>`
-           } 
+        ? `<img src="${article.urlToImage}" class="card-img-top object-fit-cover">` : `<img src="../img/no-image.png" class="card-img-top object-fit-cover">`
+      }
+           ${typeof article.content === "object" ?
+        `<p class="card-text mt-2">Please Click to Read more...</p>` : `<p class="card-text mt-2">${article.content.slice(0, 75)} ...Read more</p>`
+      } 
            <div class="card-text d-flex news-options">
              <p class="card-text me-2 pe-2 custom-border"><small class="text-muted">By : ${article.source.name}</small></p>
-             <p class="card-text "><small class="text-muted">${article.publishedAt.slice(0, 9)}</small></p>
+             <p class="card-text "><small class="text-muted">${article.publishedAt.slice(0, 10).split('-').reverse().join('-')}</small></p>
            </div>
          
          </div>
@@ -135,8 +140,8 @@ filteredArticles.forEach(article => {
          
          `;
     document.querySelector('.row').appendChild(div);
-  
-      }); 
+
+  });
 
 }
 
@@ -145,10 +150,10 @@ filteredArticles.forEach(article => {
 async function getTechNews() {
 
   const { articles } = await fetchAPIData('top-headlines?country=us&category=technology');
-  const filteredArticles = articles.filter(articles => articles.content !== '[Removed]' || articles.content ===null);
-     console.log(filteredArticles);
-     filteredArticles.forEach(article => {
-     console.log(typeof article.content);
+  const filteredArticles = articles.filter(articles => articles.content !== '[Removed]' || articles.content === null);
+  console.log(filteredArticles);
+  filteredArticles.forEach(article => {
+    console.log(typeof article.content);
     const div = document.createElement('div');
     div.classList.add('col-3');
     div.innerHTML = `
@@ -158,14 +163,14 @@ async function getTechNews() {
          <div class="card-body">
              <a href="${article.url}" target="_blank">
               ${article.urlToImage
-                ?`<img src="${article.urlToImage}" class="card-img-top object-fit-cover">`:`<img src="../img/no-image.png" class="card-img-top object-fit-cover" width="50px" height='150px'>`
-              }
-           ${typeof article.content === "object"?
-            `<p class="card-text mt-2">Please Click to Read more...</p>`:`<p class="card-text mt-2">${article.content.slice(0,75)} ...Read more</p>`
-           } 
+        ? `<img src="${article.urlToImage}" class="card-img-top object-fit-cover">` : `<img src="../img/no-image.png" class="card-img-top object-fit-cover" width="50px" height='150px'>`
+      }
+           ${typeof article.content === "object" ?
+        `<p class="card-text mt-2">Please Click to Read more...</p>` : `<p class="card-text mt-2">${article.content.slice(0, 75)} ...Read more</p>`
+      } 
            <div class="card-text d-flex news-options">
              <p class="card-text me-2 pe-2 custom-border"><small class="text-muted">By : ${article.source.name}</small></p>
-             <p class="card-text "><small class="text-muted">${article.publishedAt.slice(0, 9)}</small></p>
+             <p class="card-text "><small class="text-muted">${article.publishedAt.slice(0, 10).split('-').reverse().join('-')}</small></p>
            </div>
          
          </div>
@@ -174,8 +179,8 @@ async function getTechNews() {
          
          `;
     document.querySelector('.row').appendChild(div);
-  
-      }); 
+
+  });
 
 }
 
@@ -184,10 +189,10 @@ async function getTechNews() {
 async function getSportsNews() {
 
   const { articles } = await fetchAPIData('top-headlines?country=us&category=sports');
-  const filteredArticles = articles.filter(articles => articles.content !== '[Removed]' || articles.content ===null);
-     console.log(filteredArticles);
-     filteredArticles.forEach(article => {
-     console.log(typeof article.content);
+  const filteredArticles = articles.filter(articles => articles.content !== '[Removed]' || articles.content === null);
+  console.log(filteredArticles);
+  filteredArticles.forEach(article => {
+    console.log(typeof article.content);
     const div = document.createElement('div');
     div.classList.add('col-3');
     div.innerHTML = `
@@ -197,14 +202,14 @@ async function getSportsNews() {
          <div class="card-body">
              <a href="${article.url}" target="_blank">
               ${article.urlToImage
-                ?`<img src="${article.urlToImage}" class="card-img-top object-fit-cover">`:`<img src="../img/no-image.png" class="card-img-top object-fit-cover" width="50px" height='170px'>`
-              }
-           ${typeof article.content === "object"?
-            `<p class="card-text mt-2">Please Click to Read more...</p>`:`<p class="card-text mt-2">${article.content.slice(0,75)} ...Read more</p>`
-           } 
+        ? `<img src="${article.urlToImage}" class="card-img-top object-fit-cover">` : `<img src="../img/no-image.png" class="card-img-top object-fit-cover" width="50px" height='170px'>`
+      }
+           ${typeof article.content === "object" ?
+        `<p class="card-text mt-2">Please Click to Read more...</p>` : `<p class="card-text mt-2">${article.content.slice(0, 75)} ...Read more</p>`
+      } 
            <div class="card-text d-flex news-options">
              <p class="card-text me-2 pe-2 custom-border"><small class="text-muted">By : ${article.source.name}</small></p>
-             <p class="card-text "><small class="text-muted">${article.publishedAt.slice(0, 9)}</small></p>
+             <p class="card-text "><small class="text-muted">${article.publishedAt.slice(0, 10).split('-').reverse().join('-')}</small></p>
            </div>
          
          </div>
@@ -213,11 +218,115 @@ async function getSportsNews() {
          
          `;
     document.querySelector('.row').appendChild(div);
-  
-      }); 
+
+  });
 
 }
 
+
+//search news function
+
+async function search() {
+
+  const queryString = window.location.search;
+  // console.log(queryString);
+  const urlParams = new URLSearchParams(queryString);
+  // console.log(urlParams);
+  // console.log(urlParams.get('search-term'));
+  //so now put it in variables
+  global.search.term = urlParams.get('search-term');
+  if (global.search.term !== '' && global.search.term !== null) {
+
+    //make request anf get the result from api
+    const { articles, totalResults } = await searchAPIData();
+    // console.log(articles);
+    // console.log(articles.length);
+
+    if (articles.length===undefined) {
+      const div = document.createElement('div');
+      div.classList.add('col');
+      div.innerHTML = `
+              <div id="alert" class="alert mt-5 text-center h-50">
+                  <h2> Server Error 426 (Too Many Requests) </h2>
+              </div>
+                 
+            `;
+
+      document.querySelector('.row').appendChild(div);
+      return;
+
+    }
+    if (articles.length === 0) {
+      const div = document.createElement('div');
+      div.classList.add('col');
+      div.innerHTML = `
+              <div id="alert" class="alert mt-5 text-center h-50">
+                  <h2> Nothing Found to Your Search </h2>
+              </div>
+                 
+            `;
+
+      document.querySelector('.row').appendChild(div);
+      return;
+
+    }
+
+    //pass results so show on DOM
+    displaySearchResults(articles);
+
+
+  } else {
+    // alert('Please Enter Some Words To Search');
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+              <div id="alert" class="alert mt-5 text-center h-50">
+                  <h2> Please Enter Something To Search </h2>
+              </div>
+                 
+            `;
+
+    document.querySelector('.row').appendChild(div);
+
+
+  }
+
+}
+
+//function to display the results
+function displaySearchResults(articles) {
+
+  articles.forEach(article => {
+    console.log(typeof article.content);
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+            
+         <div class="card mb-3 border-0 mt-2">
+         <h5 class="card-title p-2 fw-bold mt-2">${article.title}</h5>
+         <div class="card-body">
+             <a href="${article.url}" target="_blank">
+              ${article.urlToImage
+        ? `<img src="${article.urlToImage}" class="card-img-top object-fit-cover">` : `<img src="../img/no-image.png" class="card-img-top object-fit-cover" width="50px" height='170px'>`
+      }
+           ${typeof article.content === "object" ?
+        `<p class="card-text mt-2">Please Click to Read more...</p>` : `<p class="card-text mt-2">${article.content.slice(0, 75)} ...Read more</p>`
+      } 
+           <div class="card-text d-flex news-options">
+             <p class="card-text me-2 pe-2 custom-border"><small class="text-muted">By : ${article.source.name}</small></p>
+             <p class="card-text "><small class="text-muted">${article.publishedAt.slice(0, 10).split('-').reverse().join('-')}</small></p>
+           </div>
+         
+         </div>
+     </a>
+       </div>
+         
+         `;
+
+    document.querySelector('.row').appendChild(div);
+  });
+
+}
 
 
 
@@ -232,6 +341,22 @@ async function fetchAPIData(endpoint) {
 
   showSpinner();
   const response = await fetch(`${API_URL}${endpoint}&apiKey=${API_KEY}`);
+
+  const data = response.json();
+  hideSpinner();
+  return data;
+
+
+
+}
+
+//search keywords 
+async function searchAPIData() {
+  const API_URL = global.api.API_URL;
+  const API_KEY = global.api.API_KEY;
+
+  showSpinner();
+  const response = await fetch(`${API_URL}everything?q=${global.search.term}&page=25&apiKey=${API_KEY}`);
 
   const data = response.json();
   hideSpinner();
@@ -299,6 +424,7 @@ function init() {
       console.log('world');
       break;
     case '/search.html':
+      search();
       console.log('search');
       break;
 
@@ -352,4 +478,4 @@ document.addEventListener("DOMContentLoaded", function () {
 //init app
 
 
-document.addEventListener('DOMContentLoaded',init);
+document.addEventListener('DOMContentLoaded', init);
